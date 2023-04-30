b6958f783bfa87b8e44356795b0a48d332308d03
Abhishekb3666 <94959131+Abhishekb3666@users.noreply.github.com>
2023-04-30 22:49:11 +0530

    Add files via upload

---
+++ Assignment 8/Part1/FactorialException.java
@@ -0,0 +1,21 @@
+package Part1;
+
+public class FactorialException extends Exception {
+    private int num;
+
+    public FactorialException(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "FactorialException:" + num + " is out of range (0-15)";
+    }
+}
